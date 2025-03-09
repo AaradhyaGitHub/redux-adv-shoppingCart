9d6ba99c1c99cca12afdbd9c4db133fb672c9dd5
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-08 22:46:43 -0800

    Implemented addItemsToCart method

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,20 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 initialCartState = {
-    items: [],
-    totalQuantity: 0,
-    totalAmount: 0
-}
+  items: [],
+  totalQuantity: 0,
+  totalAmount: 0
+};
 
 createSlice({
-    name: "cart",
-    initialState: initialCartState,
-    reducers: {
-        addItemToCart(state, action){
-            
-        },
-        removeItemFromCart(){
+  name: "cart",
+  initialState: initialCartState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
 
-        }
-    }
-})
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        });
+      } else if (existingItem) {
+        existingItem.quantity++,
+          (existingItem.totalPrice = existingItem.totalPrice + newItem.price);
+      }
+    },
+    removeItemFromCart() {}
+  }
+});
