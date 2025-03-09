80edf216ac931f2309cf2011d3b2676fb7ffa9a0
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-08 22:51:34 -0800

    Added delete Item Logic

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -22,11 +22,19 @@ createSlice({
           totalPrice: newItem.price,
           name: newItem.title
         });
-      } else if (existingItem) {
+      } else {
         existingItem.quantity++,
           (existingItem.totalPrice = existingItem.totalPrice + newItem.price);
       }
     },
-    removeItemFromCart() {}
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.item.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
+    }
   }
 });
